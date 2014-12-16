fed4bf6f09babe2d89b7faa858937bbc6ca8f121
Varun Tomar <developer@corebitss.com>
2014-12-16 12:40:19 +0530

    TransitionInSwift
    This code demonstrates how to do basic and new introduced transitions
    in iOS App using Swift language

---
+++ TrasitionInSwift/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  TrasitionInSwift
+//
+//  Created by Varun Tomar on 09/12/14.
+//  Copyright (c) 2014 Corebits. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBAction func dismissView(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
--- TrasitionInSwift/ViewController.swift
+++ TrasitionInSwift/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPopoverPresentationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +21,58 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func pushViewController(sender: AnyObject) {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        var secondViewController = SecondViewController()
+        
+        secondViewController = storyboard.instantiateViewControllerWithIdentifier("SecondViewController") as SecondViewController
+        
+        self.navigationController?.pushViewController(secondViewController, animated: true)
+        
+    }
+    
+    
+    @IBAction func presentViewController(sender: AnyObject) {
+    
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        var secondViewController = SecondViewController()
+        
+        secondViewController = storyboard.instantiateViewControllerWithIdentifier("SecondViewController") as SecondViewController
+        
+        self.navigationController?.presentViewController(secondViewController, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    @IBAction func popoverViewController(sender: AnyObject) {
+        
+        var secondViewController = SecondViewController()
+        secondViewController.modalPresentationStyle = .Popover
+        
+        secondViewController.preferredContentSize = CGSizeMake(300, 150)
+        
+        secondViewController.view.backgroundColor = UIColor.purpleColor()
+        
+        let popoverPresentationViewController = secondViewController.popoverPresentationController
+        
+        popoverPresentationViewController?.delegate = self
+        popoverPresentationViewController?.permittedArrowDirections = .Up
+        
+        popoverPresentationViewController?.sourceView = sender as UIView
+        
+        self.navigationController?.presentViewController(secondViewController, animated: true, completion: nil)
+        
+
+    }
+    
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .None
+    }
+    
 }
 
